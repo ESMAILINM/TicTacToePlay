7b14760005695d6a1dac1de2a67e9eae12e3218d
ESMAILINM <esmailinmartinez05@gmail.com>
2025-09-10 09:51:24 -0400

    Crea la entidad JugadorEntity para la base de datos local.

---
+++ app/src/main/java/edu/ucne/TicTacToePlay/tareas/local/JugadorEntity.kt
@@ -0,0 +1,11 @@
+package edu.ucne.TicTacToePlay.tareas.local
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Jugadores")
+data class JugadorEntity(
+    @PrimaryKey(autoGenerate = true)
+    val jugadorId: Int = 0,
+    val nombres: String,
+    val partidas: Int,
+)
