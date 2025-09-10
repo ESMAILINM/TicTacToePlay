e19bfe92095bc858be7254d518eb2e2430161d01
ESMAILINM <esmailinmartinez05@gmail.com>
2025-09-10 10:14:59 -0400

    Crea el caso de uso UpsertJugadorUseCase para insertar o actualizar un jugador.

---
+++ app/src/main/java/edu/ucne/TicTacToePlay/domain/usecase/UpsertJugadorUseCase.kt
@@ -0,0 +1,23 @@
+package edu.ucne.TicTacToePlay.domain.usecase
+
+import edu.ucne.TicTacToePlay.domain.model.Jugador
+import edu.ucne.TicTacToePlay.domain.repository.JugadorRepository
+import edu.ucne.TicTacToePlay.domain.validation.JugadorValidator
+import javax.inject.Inject
+
+class UpsertJugadorUseCase @Inject constructor(
+    private val repository: JugadorRepository,
+    private val validator: JugadorValidator
+) {
+    suspend operator fun invoke(jugador: Jugador): Result<Int>{
+        val nombreResult =validator.validateNombre(jugador.nombres)
+        if(!nombreResult.isValid){
+            return Result.failure(IllegalArgumentException(nombreResult.error))
+        }
+        val partidaResult = validator.validatePartida(jugador.partidas.toString())
+        if((!partidaResult.isValid)){
+            return Result.failure(IllegalArgumentException(partidaResult.error))
+        }
+        return runCatching { repository.upsertJugador(jugador) }
+    }
+}
