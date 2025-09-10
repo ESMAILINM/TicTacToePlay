17c45fd72ace98021c7150d600b3de3ceeb4c614
ESMAILINM <esmailinmartinez05@gmail.com>
2025-09-10 09:57:11 -0400

    agregar mapper entre Jugador y JugadorEntity

---
+++ app/src/main/java/edu/ucne/TicTacToePlay/tareas/mapper/JugadorMappers.kt
@@ -0,0 +1,15 @@
+package edu.ucne.TicTacToePlay.tareas.mapper
+import edu.ucne.TicTacToePlay.domain.model.Jugador
+import edu.ucne.TicTacToePlay.tareas.local.JugadorEntity
+
+fun JugadorEntity.toDomain(): Jugador = Jugador(
+    jugadorId = jugadorId,
+    nombres = nombres,
+    partidas = partidas,
+
+    )
+fun Jugador.toEntity(): JugadorEntity = JugadorEntity(
+    jugadorId = jugadorId,
+    nombres = nombres,
+    partidas = partidas,
+)
