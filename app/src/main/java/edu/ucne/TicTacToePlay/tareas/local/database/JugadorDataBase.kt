2c84ab63a3df2d373c5f29662e1f405568850799
ESMAILINM <esmailinmartinez05@gmail.com>
2025-09-24 17:35:46 -0400

    Aumenta la versión de la base de datos y agrega el DAO y la entidad LogroEntity.

--- app/src/main/java/edu/ucne/TicTacToePlay/tareas/local/database/JugadorDataBase.kt
+++ app/src/main/java/edu/ucne/TicTacToePlay/tareas/local/database/JugadorDataBase.kt
@@ -3,19 +3,24 @@ package edu.ucne.TicTacToePlay.tareas.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.TicTacToePlay.tareas.local.dao.JugadorDao
+import edu.ucne.TicTacToePlay.tareas.local.dao.LogroDao
 import edu.ucne.TicTacToePlay.tareas.local.dao.PartidaDao
 import edu.ucne.TicTacToePlay.tareas.local.entities.JugadorEntity
 import edu.ucne.TicTacToePlay.tareas.local.entities.PartidaEntity
+import edu.ucne.TicTacToePlay.tareas.local.entities.LogroEntity
 
 @Database(
     entities = [
         JugadorEntity::class,
-        PartidaEntity::class
+        PartidaEntity::class,
+        LogroEntity::class
     ],
-    version = 3,
+    version = 4,
     exportSchema = false,
 )
 abstract class JugadorDataBase : RoomDatabase() {
     abstract fun jugadorDao(): JugadorDao
     abstract fun partidaDao(): PartidaDao
+    abstract fun logroDao(): LogroDao
+
 }
