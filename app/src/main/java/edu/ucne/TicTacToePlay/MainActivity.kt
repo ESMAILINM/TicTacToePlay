74dde7c761d8183812e85daadb2ae900524c7cfe
ESMAILINM <esmailinmartinez05@gmail.com>
2025-09-27 00:10:21 -0400

    Refactoriza el código de `MainActivity` para usar `TicTacToeNavHost` en lugar de `NavHost`.

--- app/src/main/java/edu/ucne/TicTacToePlay/MainActivity.kt
+++ app/src/main/java/edu/ucne/TicTacToePlay/MainActivity.kt
@@ -6,18 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import dagger.hilt.android.AndroidEntryPoint
-import edu.ucne.TicTacToePlay.presentation.jugador.edit.EditJugadorScreen
-import edu.ucne.TicTacToePlay.presentation.jugador.list.ListJugadorScreen
-import edu.ucne.TicTacToePlay.presentation.partida.EditPartidaScreen
-import edu.ucne.TicTacToePlay.presentation.partida.list.ListPartidaScreen
+import edu.ucne.TicTacToePlay.navigation.TicTacToeNavHost
 import edu.ucne.TicTacToePlay.ui.theme.TicTacToePlayTheme
+
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,42 +24,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     val navController = rememberNavController()
-
-                    NavHost(
-                        navController = navController,
-                        startDestination = "list_jugador_screen"
-                    ) {
-                        composable("list_jugador_screen") {
-                            ListJugadorScreen(navController = navController)
-                        }
-                        composable(
-                            route = "edit_jugador_screen/{jugadorId}",
-                            arguments = listOf(
-                                navArgument("jugadorId") {
-                                    type = NavType.IntType
-                                }
-                            )
-                        ) { backStackEntry ->
-                            val jugadorId = backStackEntry.arguments?.getInt("jugadorId")
-                            EditJugadorScreen(
-                                navController = navController,
-                                jugadorId = jugadorId
-                            )
-                        }
-                        composable("list_partida_screen") {
-                            ListPartidaScreen(navController = navController)
-                        }
-                        composable(
-                            route = "edit_partida_screen/{partidaId}",
-                            arguments = listOf(navArgument("partidaId") { type = NavType.IntType })
-                        ) { backStackEntry ->
-                            val partidaId = backStackEntry.arguments?.getInt("partidaId")
-                            EditPartidaScreen(navController = navController, partidaId = partidaId)
-                        }
-
-
-                    }
-
+                    TicTacToeNavHost(navHostController = navController)
                 }
             }
         }
