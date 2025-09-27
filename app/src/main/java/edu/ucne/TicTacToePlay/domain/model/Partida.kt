a7fe2f77a9a4ae660823a2d5fd19e8da16638ad6
ESMAILINM <esmailinmartinez05@gmail.com>
2025-09-27 00:03:34 -0400

    Actualiza el campo fecha de la entidad Partida, para que obtenga la fecha actual, por defecto.

--- app/src/main/java/edu/ucne/TicTacToePlay/domain/model/Partida.kt
+++ app/src/main/java/edu/ucne/TicTacToePlay/domain/model/Partida.kt
@@ -1,8 +1,10 @@
 package edu.ucne.TicTacToePlay.domain.model
 
+import edu.ucne.TicTacToePlay.utils.getFechaActual
+
 data class Partida(
     val partidaId: Int = 0,
-    val fecha: String = "",
+    val fecha: String = getFechaActual(),
     val jugador1Id: Int,
     val jugador2Id: Int,
     val ganadorId: Int?= null,
