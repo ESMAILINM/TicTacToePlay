9481da9c89f9346a0de4054d2d14a4a51a0906b3
ESMAILINM <esmailinmartinez05@gmail.com>
2025-09-27 00:03:09 -0400

    Crea la entidad PartidaEntity y define sus relaciones con JugadorEntity

--- app/src/main/java/edu/ucne/TicTacToePlay/tareas/local/entities/PartidaEntity.kt
+++ app/src/main/java/edu/ucne/TicTacToePlay/tareas/local/entities/PartidaEntity.kt
@@ -1,10 +1,38 @@
 package edu.ucne.TicTacToePlay.tareas.local.entities
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-
-@Entity(tableName = "Partidas")
+@Entity(
+    tableName = "Partidas",
+    foreignKeys = [
+        ForeignKey(
+            entity = JugadorEntity::class,
+            parentColumns = ["jugadorId"],
+            childColumns = ["jugador1Id"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = JugadorEntity::class,
+            parentColumns = ["jugadorId"],
+            childColumns = ["jugador2Id"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = JugadorEntity::class,
+            parentColumns = ["jugadorId"],
+            childColumns = ["ganadorId"],
+            onDelete = ForeignKey.SET_NULL
+        )
+    ],
+    indices = [
+        Index("jugador1Id"),
+        Index("jugador2Id"),
+        Index("ganadorId")
+    ]
+)
 data class PartidaEntity(
     @PrimaryKey(autoGenerate = true)
     val partidaId: Int = 0,
