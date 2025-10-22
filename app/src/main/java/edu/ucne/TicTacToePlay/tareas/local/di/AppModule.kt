63b5d5d5b7acd112253dd2dc66e74bd404d5394c
ESMAILINM <esmailinmartinez05@gmail.com>
2025-10-22 11:38:32 -0400

    Agrega la configuración de Retrofit y Moshi para el consumo de la API.

--- app/src/main/java/edu/ucne/TicTacToePlay/tareas/local/di/AppModule.kt
+++ app/src/main/java/edu/ucne/TicTacToePlay/tareas/local/di/AppModule.kt
@@ -2,6 +2,8 @@ package edu.ucne.TicTacToePlay.tareas.local.di
 
 import android.content.Context
 import androidx.room.Room
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,10 +14,31 @@ import javax.inject.Singleton
 import dagger.hilt.components.SingletonComponent
 import edu.ucne.TicTacToePlay.tareas.local.dao.LogroDao
 import edu.ucne.TicTacToePlay.tareas.local.dao.PartidaDao
+import edu.ucne.TicTacToePlay.tareas.remote.TicTacToePlayApi
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 @InstallIn(SingletonComponent::class)
 @Module
 object AppModule {
+    private const val BASE_URL = "https://gestionhuacalesapi.azurewebsites.net/"
+
+    @Singleton
+    @Provides
+    fun providesMoshi(): Moshi =
+        Moshi. Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+    @Provides
+    @Singleton
+    fun providesTicTacToePlayApi(moshi: Moshi): TicTacToePlayApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(TicTacToePlayApi :: class.java)
+    }
     @Provides
     @Singleton
     fun provideJugadorDb(@ApplicationContext appContext: Context) : JugadorDataBase {
