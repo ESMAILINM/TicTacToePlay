e23b5365f8492511ca2554fc8e507fda3dcd2395
ESMAILINM <esmailinmartinez05@gmail.com>
2025-10-29 13:37:09 -0400

    Agrega mapeo entre entidades y DTOs para Jugador

--- app/src/main/java/edu/ucne/TicTacToePlay/tareas/mapper/JugadorMappers.kt
+++ app/src/main/java/edu/ucne/TicTacToePlay/tareas/mapper/JugadorMappers.kt
@@ -1,15 +1,36 @@
 package edu.ucne.TicTacToePlay.tareas.mapper
+
 import edu.ucne.TicTacToePlay.domain.model.Jugador
 import edu.ucne.TicTacToePlay.tareas.local.entities.JugadorEntity
+import edu.ucne.TicTacToePlay.tareas.remote.dto.JugadorRequest
+import edu.ucne.TicTacToePlay.tareas.remote.dto.JugadorResponse
+
 
 fun JugadorEntity.toDomain(): Jugador = Jugador(
     jugadorId = jugadorId,
+    remoteId = remoteId,
     nombres = nombres,
-    partidas = partidas,
+    email = email,
+    isPendingCreate = isPendingCreate
+)
 
-    )
 fun Jugador.toEntity(): JugadorEntity = JugadorEntity(
     jugadorId = jugadorId,
+    remoteId = remoteId,
     nombres = nombres,
-    partidas = partidas,
+    email = email,
+    isPendingCreate = isPendingCreate
 )
+
+fun JugadorResponse.toDomain(): Jugador = Jugador(
+    jugadorId = jugadorId.toString(),
+    remoteId = jugadorId,
+    nombres = nombres,
+    email = email
+)
+
+fun Jugador.toRequest(): JugadorRequest = JugadorRequest(
+    nombres = nombres,
+    email = email
+)
+
