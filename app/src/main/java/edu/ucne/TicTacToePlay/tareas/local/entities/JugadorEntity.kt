2c38d21db0627cc03e5f8a93cd2b57ec589bfb3c
ESMAILINM <esmailinmartinez05@gmail.com>
2025-10-29 13:43:35 -0400

    Modifica la entidad JugadorEntity para usar un UUID como clave primaria y añade campos para la sincronización con el servidor.

--- app/src/main/java/edu/ucne/TicTacToePlay/tareas/local/entities/JugadorEntity.kt
+++ app/src/main/java/edu/ucne/TicTacToePlay/tareas/local/entities/JugadorEntity.kt
@@ -1,12 +1,15 @@
 package edu.ucne.TicTacToePlay.tareas.local.entities
 
+import android.provider.ContactsContract
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.UUID
 
 @Entity(tableName = "Jugadores")
 data class JugadorEntity(
-    @PrimaryKey(autoGenerate = true)
-    val jugadorId: Int = 0,
+    @PrimaryKey val jugadorId: String = UUID.randomUUID().toString(),
     val nombres: String,
-    val partidas: Int,
+    val email: String,
+    val remoteId: Int? = null,
+    val isPendingCreate: Boolean = false
 )
