d9cd6b714cd189809550b35cad46171657fcd70e
ESMAILINM <esmailinmartinez05@gmail.com>
2025-10-29 14:12:09 -0400

    Convierte `ListJugadorUiEvent` en una sealed interface y agrega nuevos eventos.

--- app/src/main/java/edu/ucne/TicTacToePlay/presentation/jugador/list/ListJugadorUiEvent.kt
+++ app/src/main/java/edu/ucne/TicTacToePlay/presentation/jugador/list/ListJugadorUiEvent.kt
@@ -2,6 +2,10 @@ package edu.ucne.TicTacToePlay.presentation.jugador.list
 
 import edu.ucne.TicTacToePlay.domain.model.Jugador
 
-    interface ListJugadorUiEvent {
+    sealed interface ListJugadorUiEvent {
         data class OnDeleteJugadorClick(val jugador: Jugador) : ListJugadorUiEvent
+        data class Delete(val id: Int) : ListJugadorUiEvent
+        data object Load : ListJugadorUiEvent
+        data class ShowMessage(val message: String) : ListJugadorUiEvent
+
     }
