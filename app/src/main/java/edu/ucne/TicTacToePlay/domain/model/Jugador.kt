ee97335a2643694c9b84431c414a380fb41f9d88
ESMAILINM <esmailinmartinez05@gmail.com>
2025-10-29 13:43:12 -0400

    Modifica el modelo Jugador
    Se cambia el tipo de `jugadorId` de Int a String y se le asigna un UUID por defecto. Se agregan los campos `remoteId`, `email` y `isPendingCreate`. Se elimina el campo `partidas`.

--- app/src/main/java/edu/ucne/TicTacToePlay/domain/model/Jugador.kt
+++ app/src/main/java/edu/ucne/TicTacToePlay/domain/model/Jugador.kt
@@ -1,7 +1,11 @@
 package edu.ucne.TicTacToePlay.domain.model
 
+import java.util.UUID
+
 data class Jugador(
-    val jugadorId: Int = 0,
+    val jugadorId: String = UUID.randomUUID().toString(),
+    val remoteId: Int? = null,
     val nombres: String,
-    val partidas: Int,
+    val email : String = "",
+    val isPendingCreate: Boolean = false
 )
